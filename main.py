cc345304aacfe911f6989cd8dcba448342e17db8
ethsscan <163399017+ethsscan@users.noreply.github.com>
2024-03-14 17:42:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('sheet-parse-417019-e0b
 client = gspread.authorize(creds)
 
 # Open the Google Spreadsheet (replace 'Your Spreadsheet Name' with your actual spreadsheet name)
-spreadsheet = client.open('DCG')
+spreadsheet = client.open('ETH')
 sheet = spreadsheet.sheet1
 
 server = Server(horizon_url="https://horizon.stellar.org")
-account_id = "GDLOECLBYEQXT4QGNGK4QZAK4YFCDR6K2CNLRJWDBLB3OVMAOFBCJB3Z"
+account_id = "GDN4XX7DGOFVI3LH5HSTDIL5VJDX5TZIBHRRKMWJPDUWU7YPJRSBGU4L"
 
 def balance_handler(account_response):
     balance = account_response["balances"][1]  # Assuming XLM is the first asset in the list
